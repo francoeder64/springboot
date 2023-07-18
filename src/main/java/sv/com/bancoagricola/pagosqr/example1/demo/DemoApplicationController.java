3a4c37d769f67ec8c57f4b30f477ac1cdd45a76d
Franco Eder Chacon Linares <francoechacon@gmail.com>
2023-07-17 22:10:06 -0600

    Initial commit 1.0
    Changes to be committed:
    	new file:   .gitignore
    	new file:   build.gradle
    	new file:   gradle/wrapper/gradle-wrapper.jar
    	new file:   gradle/wrapper/gradle-wrapper.properties
    	new file:   gradlew
    	new file:   gradlew.bat
    	new file:   src/main/java/sv/com/bancoagricola/pagosqr/example1/demo/DemoApplicationController.java
    	new file:   src/main/resources/application.properties
    	new file:   src/test/java/sv/com/bancoagricola/pagosqr/example1/demo/DemoApplicationTests.java
    
    date 17/07/2023
    author Franco Eder Chacon Linares

---
+++ src/main/java/sv/com/bancoagricola/pagosqr/example1/demo/DemoApplicationController.java
@@ -0,0 +1,13 @@
+package sv.com.bancoagricola.pagosqr.example1.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoApplicationController {
+    
+    @RequestMapping("/hello") 
+    public String hello (){
+        return "Hello world. This is an example using spring boot with visual code and also I'm testing git.";
+    }
+}
